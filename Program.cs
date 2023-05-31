e1d9ec97c985a7ffbd3edf5c8733d68eaab56b8d
Zarylbekov22 <128897608+Zarylbekov22@users.noreply.github.com>
2023-05-31 14:15:19 +0300

    Example003
    Example003

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿static uint Akk(uint n, uint m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akk(n - 1, 1);
+    else
+        return Akk(n - 1, Akk(n, m - 1));
+}
+ 
+Console.WriteLine(Akk(1, 2)); // 4
+Console.WriteLine(Akk(0, 0)); // 1
+Console.WriteLine(Akk(4, 5)); // Stack Overflow !
+ 
+Console.ReadKey();
